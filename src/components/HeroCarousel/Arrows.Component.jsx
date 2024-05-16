e4f55be69ba7cf4a6fc953654725b9e233008376
rohandevtown <rohankinnal@devtown.in>
2024-05-16 19:17:24 +0530

    d3

---
+++ src/components/HeroCarousel/Arrows.Component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export function NextArrow(props){
+    return (
+        <>
+            <div className={props.className} style={{...props.style}} onClick={props.onclick}>
+
+            </div>
+        </>
+    )
+}
+
+export function PrevArrow(props){
+    return (
+        <>
+            <div className={props.className} style={{...props.style}} onClick={props.onclick}>
+
+            </div>
+        </>
+    )
+}
